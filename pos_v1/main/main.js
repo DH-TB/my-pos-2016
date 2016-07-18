0efed2111478318b0494d4cdac6db1950d4f5f69
Hao Hu <hhu@thoughtworks.com>
2016-07-19 01:08:47 +0800

    finish

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -7,6 +7,12 @@ function printReceipt(tags) {
 
   const allPromotions = loadPromotions();
   const receiptItems = buildReceiptItems(cartItems, allPromotions);
+
+  const receipt = buildReceipt(receiptItems);
+
+  const receiptText = buildReceiptText(receipt);
+
+  console.log(receiptText);
 }
 
 function buildCartItems(tags, allItems) {
@@ -30,4 +36,74 @@ function buildCartItems(tags, allItems) {
   }
 
   return cartItems;
+}
+
+function buildReceiptItems(cartItems, allPromotions) {
+
+  const receiptItems = [];
+
+  for (const cartItem of cartItems) {
+
+    const type = findPromotionType(cartItem.item.barcode, allPromotions);
+
+    let subTotal = cartItem.item.price * cartItem.count;
+
+    let saved = 0;
+
+    if (type === 'BUY_TWO_GET_ONE_FREE') {
+      saved = parseInt(cartItem.count / 3) * cartItem.item.price;
+      subTotal -= saved;
+    }
+
+    receiptItems.push({cartItem: cartItem, saved: saved, subTotal: subTotal});
+  }
+
+  return receiptItems;
+}
+
+function findPromotionType(barcode, promotions) {
+
+  const promotion = promotions.find(promotion => promotion.barcodes.some(b => b === barcode));
+
+  if (promotion) {
+    return promotion.type;
+  }
+}
+
+function buildReceipt(receiptItems) {
+
+  let total = 0;
+  let savedTotal = 0;
+
+  for (const receiptItem of receiptItems) {
+    total += receiptItem.subTotal;
+    savedTotal += receiptItem.saved;
+  }
+
+  return {
+    receiptItems: receiptItems,
+    total: total,
+    savedTotal: savedTotal
+  }
+}
+
+function buildReceiptText(receipt) {
+
+  let receiptItemsText = '';
+
+  for (const receiptItem of receipt.receiptItems) {
+    const cartItem = receiptItem.cartItem;
+    receiptItemsText += `名称：${cartItem.item.name}，数量：${cartItem.count}${cartItem.item.unit}，单价：${formatPrice(cartItem.item.price)}(元)，小计：${formatPrice(receiptItem.subTotal)}(元)\n`;
+  }
+
+  return `***<没钱赚商店>收据***
+${receiptItemsText}\
+----------------------
+总计：${formatPrice(receipt.total)}(元)
+节省：${formatPrice(receipt.savedTotal)}(元)
+**********************`;
+}
+
+function formatPrice(price) {
+  return price.toFixed(2);
 }
